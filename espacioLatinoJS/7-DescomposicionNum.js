81425d3d7265c7a80a1d75b8d428df818866c1c3
LeonardoBrabo <leonardo.david.brabo@gmail.com>
2022-08-31 15:55:28 -0300

    ejer 7 decomposicion de numeros en proceso

---
+++ espacioLatinoJS/7-DescomposicionNum.js
@@ -0,0 +1,23 @@
+
+const descomponerNumero = numero =>{
+    let unidad,decena,centena,unidemil;
+
+    unidad = numero % 10;
+    decena = Math.trunc(numero / 10);
+    centena = Math.trunc(numero / 100);
+    unidemil = Math.trunc(numero / 1000);
+
+    (numero < 10)
+        ? console.log("unidad: ", numero)
+        : (numero < 100)
+            ? console.log(`Decena: ${decena}, Unidad: ${unidad}`)
+            :(numero < 1000)
+                ? console.log(`Centena: ${centena} Decena: ${decena}, Unidad: ${unidad}`)
+                :(numero < 10000)
+                    ? console.log(`Unidad de Mil:${unidemil} Centena: ${centena} Decena: ${decena}, Unidad: ${unidad}`)
+                    : console.log("se paso de rango numerico");
+}
+
+descomponerNumero(1456);
+
+ //arreglar.
