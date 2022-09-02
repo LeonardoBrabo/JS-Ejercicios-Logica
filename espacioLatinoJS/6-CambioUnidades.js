210d7b5cf407f18148f0b3d17226bdb969e9a928
LeonardoBrabo <leonardo.david.brabo@gmail.com>
2022-09-02 20:15:22 -0300

    arreglado ejer 6 y 7

--- espacioLatinoJS/6-CambioUnidades.js
+++ espacioLatinoJS/6-CambioUnidades.js
@@ -1,16 +1,19 @@
 const prompt = require('prompt-sync')();
 
 const CambioUnidadTiempo = (horas,minutos)=>{
-    
 
+    //evitar que ingrese otros tipos de caracteres
+    if(!(/^[0-9]{1,2}$/g.test(horas))) return console.log(`las horas: ${horas} está en un formato erroneo, ingrese solo numeros enteros.`);
+    if(!(/^[0-9]{1,2}$/g.test(minutos))) return console.log(`Los minutos: ${minutos} está en un formato erroneo, ingrese solo numeros enteros.`);
+    
     let hours = parseInt(horas) 
     let minutes = parseInt(minutos);
     let cambioASegundos = (hours*60*60)+(minutes*60);
 
-    return ` ${horas}:${minutos} en segundo es: ${cambioASegundos}`
+    return console.log(` ${horas}:${minutos} en segundo es: ${cambioASegundos}`)
 
     
 }
 
-console.log(CambioUnidadTiempo(prompt("ingrese una hora: "),prompt("ingrese los minutos: ")));
+CambioUnidadTiempo(prompt("ingrese una hora: "),prompt("ingrese los minutos: "));
 
--- espacioLatinoJS/7-DescomposicionNum.js
+++ espacioLatinoJS/7-DescomposicionNum.js
@@ -1,23 +1,30 @@
 
-const descomponerNumero = numero =>{
+const descomponerNumero = (numero= undefined) =>{
     let unidad,decena,centena,unidemil;
 
-    unidad = numero % 10;
+    unidad = Math.trunc(numero % 10);
     decena = Math.trunc(numero / 10);
     centena = Math.trunc(numero / 100);
     unidemil = Math.trunc(numero / 1000);
 
+    if(numero === undefined) return console.log("No ingreso ningun número");
+    if(typeof numero != "number") return console.log(`Lo ingresado no es un número`);
+    if(Math.sign(numero) === -1) return console.log("ingrese solo numeros positivos");
+
     (numero < 10)
-        ? console.log("unidad: ", numero)
+        ? console.log(`Unidad: ${unidad}`)
         : (numero < 100)
             ? console.log(`Decena: ${decena}, Unidad: ${unidad}`)  
             :(numero < 1000)
-                ? console.log(`Centena: ${centena} Decena: ${decena}, Unidad: ${unidad}`)
+                ? console.log(`Centena: ${centena} Decena: ${decena%10}, Unidad: ${unidad}`)
                 :(numero < 10000)
-                    ? console.log(`Unidad de Mil:${unidemil} Centena: ${centena} Decena: ${decena}, Unidad: ${unidad}`)
-                    : console.log("se paso de rango numerico");
+                    ? console.log(`Unidad de Mil:${unidemil} Centena: ${centena%10} Decena: ${decena%10}, Unidad: ${unidad}`)
+                    : console.log("se paso de rango numerico, solo analiza hasta unidades de mil");
+
+    if(numero % 1 !== 0){
+        console.log(`Parte decimal: ${(numero - (Math.trunc(numero))).toFixed(2)}`); //muestra la parte decimal si es que tiene.
+    }
 }
 
-descomponerNumero(1456);
+descomponerNumero(1254.894);
 
-//realiza mal el calculo
